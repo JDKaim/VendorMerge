bfd83bd443cb076c0a6c57de4de5498677bec3ae
JD Kaim <jdkaim@hotmail.com>
2023-08-15 21:26:04 -0700

    first commit

---
+++ entities/CustomerVendorRecord.cs
@@ -0,0 +1,30 @@
+public class CustomerVendorRecord
+{
+
+    private Dictionary<string, int> _quantities = new Dictionary<string, int>();
+
+    public readonly string Vendor;
+    public readonly string Customer;
+
+    public CustomerVendorRecord(string vendor, string customer)
+    {
+        this.Vendor = vendor;
+        this.Customer = customer;
+    }
+
+    public void AddQuantity(string product, int quantity)
+    {
+        if (quantity < 0) { throw new ArgumentOutOfRangeException("Cannot be a negative quantity."); }
+        if (!this._quantities.ContainsKey(product))
+        {
+            this._quantities.Add(product, 0);
+        }
+        this._quantities[product] += quantity;
+    }
+
+    public int GetQuantity(string product)
+    {
+        if (!this._quantities.ContainsKey(product)) { return 0; }
+        return this._quantities[product];
+    }
+}
